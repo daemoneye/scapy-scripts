bfa196d8c708ae7c4ac2463227cb1cd32ec6ac31
daemoneye <daemoneye2@gmail.com>
2020-04-25 20:57:25 -0400

    Allow for the user to specify a DNS server

--- dns.py
+++ dns.py
@@ -2,14 +2,14 @@
 import argparse
 from scapy.all import *
 
-def get_IPv4(hostname):
+def get_IPv4(hostname, dns):
     IPv4 = ''
     
     # Localhost is always 127.0.0.1
     if hostname == 'localhost':
         IPv4 = '127.0.0.1'
     else:
-        answer = sr1(IP(dst="8.8.8.8")/UDP(sport=RandShort(), dport=53)/DNS(rd=1,qd=DNSQR(qname=hostname,qtype="A")))
+        answer = sr1(IP(dst=dns)/UDP(sport=RandShort(), dport=53)/DNS(rd=1,qd=DNSQR(qname=hostname,qtype="A")))
         IPv4 = str(answer.an.rdata)
     return IPv4
 
@@ -18,11 +18,18 @@ def parser():
     parser.add_argument('-H', '--hostname',
                         dest='host',
                         help='Set hostname')
+    parser.add_argument('-d', '--dns',
+                        dest='dns',
+                        help='Set DNS server')
     return parser.parse_args()
 
 def main():
     args = parser()
-    print(get_IPv4(args.host))
+    hostname = args.host
+    dns = args.dns
+    if dns == None:
+        dns = '8.8.8.8'
+    print(get_IPv4(hostname, dns))
 
 if __name__ == "__main__":
     main()
