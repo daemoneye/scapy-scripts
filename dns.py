02e9d69c191c18fe9ccaaf8c9b83f714606af392
daemoneye <daemoneye2@gmail.com>
2020-04-25 20:50:33 -0400

    Added argparse and option to set hostname

--- dns.py
+++ dns.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import argparse
 from scapy.all import *
 
 def get_IPv4(hostname):
     IPv4 = ''
+    
+    # Localhost is always 127.0.0.1
     if hostname == 'localhost':
         IPv4 = '127.0.0.1'
     else:
@@ -10,12 +13,16 @@ def get_IPv4(hostname):
         IPv4 = str(answer.an.rdata)
     return IPv4
 
+def parser():
+    parser = argparse.ArgumentParser(description='Query Google\'s DNS server for the IPv4 address of a hostname')
+    parser.add_argument('-H', '--hostname',
+                        dest='host',
+                        help='Set hostname')
+    return parser.parse_args()
+
 def main():
-    address1 = "localhost"
-    address2 = "www.daemo.nz"
-    
-    print(address2 + " = " + get_IPv4(address2))
-    print(address1 + " = " + get_IPv4(address1))
+    args = parser()
+    print(get_IPv4(args.host))
 
 if __name__ == "__main__":
     main()
